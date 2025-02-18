86d5b3a3baa0beb0aefc197c130c4af29e228943
Shashwat Mohanta <shashwatmohanta@ip-192-168-0-109.ap-south-1.compute.internal>
2025-02-18 11:46:31 +0530

    model training changes

--- ds-assignment-master/src/models/classifier.py
+++ ds-assignment-master/src/models/classifier.py
@@ -1,10 +1,12 @@
 from abc import ABC, abstractmethod
 from typing import Dict, List
-
+from catboost import Pool
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator
-
+from sklearn.metrics import (
+    accuracy_score, precision_score, recall_score, f1_score,roc_auc_score
+)
 
 class Classifier(ABC):
     @abstractmethod
@@ -22,20 +24,30 @@ class Classifier(ABC):
 
 class SklearnClassifier(Classifier):
     def __init__(
-        self, estimator: BaseEstimator, features: List[str], target: str,
+        self, estimator: BaseEstimator, features: List[str], target: str
     ):
         self.clf = estimator
         self.features = features
         self.target = target
 
     def train(self, df_train: pd.DataFrame):
-        self.clf.fit(df_train[self.features].values, df_train[self.target].values)
+        print(df_train[self.features].isna().sum())
+        X_train_pooled=Pool(data=df_train[self.features], label=df_train[self.target])
+        self.clf.fit(X_train_pooled)
 
     def evaluate(self, df_test: pd.DataFrame):
-        raise NotImplementedError(
-            f"You're almost there! Identify an appropriate evaluation metric for your model and implement it here. "
-            f"The expected output is a dictionary of the following schema: {{metric_name: metric_score}}"
-        )
+        metrics_dict={}
+        df_test['preds']=self.clf.predict(df_test[self.features],prediction_type='Class')
+        metrics_dict["accuracy"] = accuracy_score(df_test[self.target], df_test['preds'])
+        metrics_dict["precision_weighted"] = precision_score(df_test[self.target], df_test['preds'], average="weighted")
+        metrics_dict["recall_weighted"] = recall_score(df_test[self.target], df_test['preds'], average="weighted")
+        metrics_dict["f1_weighted"] = f1_score(df_test[self.target], df_test['preds'], average="weighted")
+        metrics_dict["roc_auc"] = roc_auc_score(df_test[self.target], df_test['preds'])
+        
+        return metrics_dict
 
     def predict(self, df: pd.DataFrame):
-        return self.clf.predict_proba(df[self.features].values)[:, 1]
+        print(df.shape)
+        print(df.isna().sum())
+        print(df[df["n_requests"].isna()].head())
+        return self.clf.predict(df[self.features],prediction_type='Probability')[:, 1]
--- ds-assignment-master/src/models/train_model.py
+++ ds-assignment-master/src/models/train_model.py
@@ -1,6 +1,7 @@
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-
+from catboost import CatBoostClassifier,Pool
 from src.models.classifier import SklearnClassifier
 from src.utils.config import load_config
 from src.utils.guardrails import validate_evaluation_metrics
@@ -15,8 +16,13 @@ def main():
     df = store.get_processed("transformed_dataset.csv")
     df_train, df_test = train_test_split(df, test_size=config["test_size"])
 
-    rf_estimator = RandomForestClassifier(**config["random_forest"])
-    model = SklearnClassifier(rf_estimator, config["features"], config["target"])
+    class_counts = np.bincount(df_train[config["target"]])
+    total_samples = len(df_train)
+    class_weights = [total_samples / (len(class_counts) * count) for count in class_counts]
+    class_weights[1]=class_weights[1]*1.5
+
+    rf_estimator = CatBoostClassifier(**config["catboost"],class_weights=class_weights)
+    model = SklearnClassifier(rf_estimator, config["catboost_features"], config["target"])
     model.train(df_train)
 
     metrics = model.evaluate(df_test)
