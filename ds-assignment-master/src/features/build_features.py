2c00875ee5860593c4aee9422bb93e15c94fe341
Shashwat Mohanta <shashwatmohanta@ip-192-168-0-109.ap-south-1.compute.internal>
2025-02-18 11:33:44 +0530

    feature engineering changes

--- ds-assignment-master/src/features/build_features.py
+++ ds-assignment-master/src/features/build_features.py
@@ -3,8 +3,9 @@ from sklearn.model_selection import train_test_split
 
 from src.features.transformations import (
     driver_distance_to_pickup,
+    total_distance,
     driver_historical_completed_bookings,
-    hour_of_day,
+    hour_of_day
 )
 from src.utils.store import AssignmentStore
 
@@ -13,16 +14,19 @@ def main():
     store = AssignmentStore()
 
     dataset = store.get_processed("dataset.csv")
-    dataset = apply_feature_engineering(dataset)
+    participant_past_perf = store.get_processed("participant_past_performance.csv")
+    participant_distance_preference = store.get_processed("participant_distance_preference.csv")
+    dataset = apply_feature_engineering(dataset,participant_past_perf,participant_distance_preference)
 
     store.put_processed("transformed_dataset.csv", dataset)
 
 
-def apply_feature_engineering(df: pd.DataFrame) -> pd.DataFrame:
+def apply_feature_engineering(df: pd.DataFrame,participant_past_perf: pd.DataFrame,participant_distance_preference: pd.DataFrame) -> pd.DataFrame:
     return (
         df.pipe(driver_distance_to_pickup)
+        .pipe(total_distance)
         .pipe(hour_of_day)
-        .pipe(driver_historical_completed_bookings)
+        .pipe(driver_historical_completed_bookings,participant_past_perf,participant_distance_preference)
     )
 
 
--- ds-assignment-master/src/features/transformations.py
+++ ds-assignment-master/src/features/transformations.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from haversine import haversine
 
@@ -14,14 +15,34 @@ def driver_distance_to_pickup(df: pd.DataFrame) -> pd.DataFrame:
     )
     return df
 
+def total_distance(df: pd.DataFrame) -> pd.DataFrame:
+    df["total_distance"]=df["driver_distance"]+df["trip_distance"]
+    return df
 
 def hour_of_day(df: pd.DataFrame) -> pd.DataFrame:
     df["event_hour"] = df["event_timestamp"].apply(robust_hour_of_iso_date)
     return df
 
 
-def driver_historical_completed_bookings(df: pd.DataFrame) -> pd.DataFrame:
-    raise NotImplementedError(
-        f"Show us your feature engineering skills! Suppose that drivers with a good track record are more likely to accept bookings. "
-        f"Implement a feature that describes the number of historical bookings that each driver has completed."
-    )
+def driver_historical_completed_bookings(df: pd.DataFrame,participant_past_perf: pd.DataFrame, participant_distance_preference: pd.DataFrame) -> pd.DataFrame:
+    df['event_timestamp'] = pd.to_datetime(df['event_timestamp'],errors='coerce', utc=True)
+    participant_past_perf['event_timestamp'] = pd.to_datetime(participant_past_perf['event_timestamp'],errors='coerce', utc=True)
+    participant_distance_preference['event_timestamp'] = pd.to_datetime(participant_distance_preference['event_timestamp'],errors='coerce', utc=True)
+    df=df.sort_values('event_timestamp').reset_index(drop=True)
+    participant_past_perf=participant_past_perf.sort_values('event_timestamp').reset_index(drop=True)
+    participant_distance_preference=participant_distance_preference.sort_values('event_timestamp').reset_index(drop=True)
+    df=pd.merge_asof(df, participant_past_perf, 
+                          on="event_timestamp", 
+                          by="driver_id",
+                          direction="backward")
+
+    
+    df=pd.merge_asof(df, participant_distance_preference, 
+                          on="event_timestamp", 
+                          by="driver_id",
+                          direction="backward")
+
+
+    df=df.fillna(0)
+
+    return df
